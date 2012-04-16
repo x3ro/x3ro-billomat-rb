b7b7271c1cae25675ea6353dec292f08798a311e
Lucas Jenss <lucas@x3ro.de>
2012-04-16 20:52:40 +0200

    Remove HTTP authentication as it is not supported anymore
    The API docs (http://www.billomat.com/en/api/basics/) list the api key
    as the only authentication method available

--- test/test_billomat.rb
+++ test/test_billomat.rb
@@ -5,8 +5,6 @@ class BillomatTest < Test::Unit::TestCase
   def test_module_properties
     assert_equal Billomat.account, nil
     assert_equal Billomat.key, nil
-    assert_equal Billomat.email, nil
-    assert_equal Billomat.password, nil
 
     x = Billomat.account = "test"
     assert_equal x, "test"
@@ -15,17 +13,6 @@ class BillomatTest < Test::Unit::TestCase
     x = Billomat.key = "test"
     assert_equal x, "test"
     assert_equal Billomat.key, "test"
-
-    x = Billomat.authenticate("foo@bar.de", "test")
-    assert_equal Billomat.email, "foo@bar.de"
-    assert_equal Billomat.password, "test"
-    assert_equal Billomat.key, nil # Key is reset when email/password auth is used
-
-    x = Billomat.key = "test"
-    assert_equal x, "test"
-    assert_equal Billomat.key, "test"
-    assert_equal Billomat.email, nil # Email is reset when key-auth is used
-    assert_equal Billomat.password, nil # Password is reset when key-auth is used
   end
 end
 
