1e6274105232300248421c0ca07831d78c5ab898
Lucas Jenss <lucas@x3ro.de>
2012-05-02 22:25:38 +0200

    Add mixin module for resources that have the "myself" dataset

--- lib/billomat/resource_types.rb
+++ lib/billomat/resource_types.rb
@@ -20,4 +20,47 @@ module Billomat
       end
     end
   end
+
+
+
+  # The "myself" record is a special record provided by the Billomat API, which represents
+  # the data for the user with whose credentials the API is being accessed. This special
+  # record type currently exists for:
+  #
+  # * [Clients](http://www.billomat.com/en/api/clients/)
+  # * [Users](http://www.billomat.com/en/api/users/)
+  #
+  # It appears that these special records can not be saved through the API, which is
+  # why this mixin exists.
+  #
+  module ResourceWithMyselfRecord
+
+    # This field indicates whether or not the record is the "myself" record.
+    # We need this because the "myself" record can not be edited (yet?).
+    attr_accessor :myselfRecord
+
+    def initialize(*args)
+      @myselfRecord = false
+      super *args
+    end
+
+
+    def isMyselfRecord?
+      @myselfRecord
+    end
+
+
+    def save(*args)
+      raise NoMethodError, "Cannot save #{self.class.name}.myself over Billomat API" if isMyselfRecord?
+      super *args
+    end
+
+
+    def destroy(*args)
+      raise NoMethodError, "Cannot destroy #{self.class.name}.myself over Billomat API" if isMyselfRecord?
+      super *args
+    end
+
+  end
+
 end
