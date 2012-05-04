66dfee6df5c6a3d28e8d56c20288bdefa8d39df4
Lucas Jenss <lucas@x3ro.de>
2012-05-05 00:40:34 +0200

    Test file cleanup and auto-generated default resource tests (hooray for DRY)

--- test/test_billomat.rb
+++ test/test_billomat.rb
@@ -9,6 +9,48 @@ abort("Bye!") if $stdin.gets != "yes\n"
 
 class BillomatTest < Test::Unit::TestCase
 
+  # This method creates a basic functionality test for a given resource.
+  # The following things are tested:
+  #
+  #   * Is the resource readable, that is, can we access records from it
+  #   * Can we create new records of this resource
+  #   * Can we edit a certain field in this resource
+  #
+  def self.default_resource_test(resource, test_field, test_value)
+    # Default test for reading a resource
+    define_method "test_read_#{resource}_resource".to_sym do
+      x = Billomat.res(resource).find(:all)
+      assert x.is_a? Array
+
+      x = Billomat.res(resource).first
+      assert x.is_a? Billomat.res(resource)
+
+      x = Billomat.res(resource).last
+      assert x.is_a? Billomat.res(resource)
+    end
+
+    # Default test for creating a resource
+    define_method "test_create_#{resource}_resource".to_sym do
+      x = Billomat.res(resource).new
+      assert x.save, "Could not save newly created #{resource}"
+      assert x.save, "#{resource} creation resulted in invalid #{resource} record"
+      assert(x.id > 0, "New #{resource} was not successfully created (no id)")
+
+      y = Billomat.res(resource).find(x.id)
+      y.attributes[test_field] = test_value
+      assert y.save, "Editing #{resource} resource did not succeed"
+
+      z = Billomat.res(resource).find(x.id)
+      assert_equal test_value, z.attributes[test_field]
+    end
+  end
+
+
+
+  #
+  # Prepare the tests
+  #
+
   def setup
     # Make sure API credentials for the unit tests are supplied
     if ENV['ACC'].nil? or ENV['KEY'].nil?
@@ -33,6 +75,14 @@ class BillomatTest < Test::Unit::TestCase
 
 
 
+  #
+  # Let the tests begin!
+  #
+
+
+
+  # Test API connectivity and basic functionality
+
   def test_billomat_connectivity
     assert Billomat.validate!, "Testing if we're able to connect to Billomat using the supplied API credentials"
   end
@@ -53,6 +103,16 @@ class BillomatTest < Test::Unit::TestCase
     assert_equal @apiKey, Billomat.key
   end
 
+
+  def test_api_resource_getter
+    Billomat::Resources.constants.each do |klass|
+      assert_equal(Billomat::Resources.const_get(klass), Billomat.res(klass), "Billomat#res seems to be broken")
+    end
+  end
+
+
+
+  # Test client resource
 
   def test_read_client_resource
     x = Billomat.res(:client).find(:all)
@@ -96,6 +156,9 @@ class BillomatTest < Test::Unit::TestCase
   end
 
 
+
+  # Test settings resource
+
   def test_read_setting_resource
     x = Billomat.res(:settings).find
     assert x.is_a? Billomat.res(:settings)
@@ -113,11 +176,10 @@ class BillomatTest < Test::Unit::TestCase
   end
 
 
-  def test_api_resource_getter
-    Billomat::Resources.constants.each do |klass|
-      assert_equal(Billomat::Resources.const_get(klass), Billomat.res(klass), "Billomat#res seems to be broken")
-    end
-  end
+
+  # Test article resource
+
+  default_resource_test :article, :description, "test1234"
 
 end
 
