1c93911cdf0d19d9a50606f7823a390d774facce
Lucas Jenss <lucas@x3ro.de>
2012-05-05 01:45:18 +0200

    Create additional exception for when plan quota is exceeded

--- lib/billomat/exceptions.rb
+++ lib/billomat/exceptions.rb
@@ -1,4 +1,5 @@
 module Billomat
   class MethodNotAvailable < StandardError; end
   class NotInitializedError < StandardError; end
+  class QuotaExceededError < ActiveResource::ForbiddenAccess; end
 end
