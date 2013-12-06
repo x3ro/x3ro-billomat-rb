55bd2e43e61d414e9f9f105f17665fdf1e0cdccf
Max Schulze <max@mo-stud.io>
2013-12-06 14:27:03 +0100

    quickly add a new invoice payment resource

---
+++ lib/billomat/resources/invoice_payment.rb
@@ -0,0 +1,12 @@
+module Billomat::Resources
+
+=begin
+Implements [Billomat Invoice Payments](http://www.billomat.com/en/api/invoice_payments/)
+=end
+  class InvoicePayment < Billomat::Base
+  end
+
+end
+
+
+
