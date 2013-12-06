6caa4b3f3c4ae402c5a2240368df8ee455f18478
Max Schulze <max@mo-stud.io>
2013-12-06 14:30:54 +0100

    respect the dash syntax from billomat

--- lib/billomat/resources/invoice_payment.rb
+++ lib/billomat/resources/invoice_payment.rb
@@ -4,6 +4,7 @@ module Billomat::Resources
 Implements [Billomat Invoice Payments](http://www.billomat.com/en/api/invoice_payments/)
 =end
   class InvoicePayment < Billomat::Base
+    self.element_name = 'invoice-payment'
   end
 
 end
